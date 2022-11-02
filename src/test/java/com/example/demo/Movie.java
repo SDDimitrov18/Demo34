ad9ef39e1c0d1c30a24797b8745ebb604c0caf08
SDDimitrov18 <SDDimitrov18@codingburgas.bg>
2022-11-02 15:40:10 +0200

    Initial commit

---
+++ src/test/java/com/example/demo/Movie.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+public class Movie {
+    int id;
+    String name;
+    double rating;
+    public Movie(){
+
+    }
+    public Movie(int id, String name, double rating){
+        super();
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String toString()
+    {
+        return "Movie [id = " + id + ", name=" + name + ", rating=" + rating + "]";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
